0f05bf1950a831bd28605c6b72dce32823e285e5
Anton <64681199+TonyCardio@users.noreply.github.com>
2020-05-04 21:19:44 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace TowerDefence
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //Ты пидор
         }
     }
 }
