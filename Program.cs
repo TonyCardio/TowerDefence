c15254079e6b004aab9b53f473a8c4d03dc9da5b
stimulk08 <63906786+stimulk08@users.noreply.github.com>
2020-05-04 21:22:06 +0500

    .

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace TowerDefence
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //Ты пидор
+            //Pidor
         }
     }
 }
