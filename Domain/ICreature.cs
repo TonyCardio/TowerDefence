a605237da87aad55f2f54ec3ebdf2cfd7ca8d34d
TonyCardio <profiswimmer@yandex.ru>
2020-05-09 11:14:39 +0500

    Добавлены - Интерфейс ICreature (сущность объектов на поле, принимающих участие в игровых действиях) - Класс MovingCommand (перемещение объекта на поле)

---
+++ Domain/ICreature.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TowerDefence.Domain
+{
+    public interface ICreature
+    {
+        MovingCommand Act(int x, int y);
+        Action ActionInConflict(ICreature conflictedObject);
+        // Dispose для пули DecreaseHealth для Enemy
+    }
+
+}
