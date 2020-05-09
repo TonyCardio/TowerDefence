9cd38e1063af59a8c65207de73048a25350b0f9a
Ilya_Shumilin <i.sh-vk@yandex.ru>
2020-05-09 19:16:18 +0500

    Внес коррективы в Sprite

--- View/Sprite.cs
+++ View/Sprite.cs
@@ -9,6 +9,24 @@ namespace TowerDefence.View
 {
     class Sprite
     {
-        public Bitmap Texture { get; private set; }
+        public Bitmap TextureInRect { get; private set; }
+        public Point Position { get; private set; }
+        Rectangle textureRectangle;
+        Bitmap texture;
+
+        //Тут хз, может бытть лучше будет путь до текстуры передавать
+        public void SetTexture(Bitmap texture)
+        {
+            this.texture = texture;
+        }
+        public void SetTextureRect(Rectangle rectangle)
+        {
+            textureRectangle = rectangle;
+            TextureInRect = texture.Clone(rectangle, texture.PixelFormat);
+        }
+        public void SetPosition(Point point)
+        {
+            Position = point;
+        }
     }
 }
