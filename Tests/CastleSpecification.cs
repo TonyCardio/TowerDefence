3001e87a1fac84d396000f0924bd968297256dda
TonyCardio <profiswimmer@yandex.ru>
2020-05-24 14:41:14 +0500

    Небольшие доработки

--- Tests/CastleSpecification.cs
+++ Tests/CastleSpecification.cs
@@ -14,11 +14,11 @@ namespace TowerDefence.Tests
     [TestFixture]
     class CastleSpecification
     {
-         [Test]
-         public void CastleIsNotDestroyed_whenItCreated()
+        [Test]
+        public void CastleIsNotDestroyed_whenItCreated()
         {
             var castle = new Castle();
-            castle.IsAlive.Should().BeFalse();
+            castle.IsAlive.Should().BeTrue();
         }
 
         [Test]
@@ -27,12 +27,12 @@ namespace TowerDefence.Tests
             var castle = new Castle();
             var pathSpawnToCastle = new List<Point> { new Point(0, 0), new Point(0, 1), new Point(1, 1) };
             var enemy = new HighSkeleton(pathSpawnToCastle);
-            for(var i=0; i< 10; i++)
+            for (var i = 0; i < 10; i++)
             {
                 castle.ActionInConflict(enemy)();
             }
             castle.Health.Should().Be(0);
-            castle.IsAlive.Should().BeTrue();
+            castle.IsAlive.Should().BeFalse();
         }
     }
 }
--- Tests/IntegrationTests.cs
+++ Tests/IntegrationTests.cs
@@ -10,6 +10,19 @@ namespace TowerDefence.Tests
     [TestFixture]
     class IntegrationTests
     {
-
+        [Test]
+        public void Test()
+        {
+            var validLines = new[] { "000\r\n321\r\n000\r\n", "1" };
+            var level = LevelsLoader.LoadLevelFromLines(validLines, "Level");
+            var state = new FieldState(level.Field);
+            var spawn = level.Field.EnemySpawnPos;
+            var monster = new GreenMonster(level.PathSpawnToCastle);
+            level.EnemiesPerWave = 1;
+            level.Field.Cells[spawn.X, spawn.Y].Creature = monster;
+            state.BeginAct();
+            state.EndAct();
+            monster.Position.Should().Be(level.PathSpawnToCastle[1]);
+        }
     }
 }
